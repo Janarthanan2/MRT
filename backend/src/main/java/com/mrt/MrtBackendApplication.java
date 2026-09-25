ed4f0cfd1131900d7faab2216e84fdf0b1fc3257
Janarthanan V K <119393515+Janarthanan2@users.noreply.github.com>
2026-09-25 21:31:37 +0530

    fix: remove duplicate backend application entry point

--- backend/src/main/java/com/mrt/MrtBackendApplication.java
+++
@@ -1,4 +0,0 @@
-package com.mrt;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication public class MrtBackendApplication { public static void main(String[] args){SpringApplication.run(MrtBackendApplication.class,args);} }
